5d19e8f94e7997134ce3f4788bdaba5164e605b4
vicysan-126 <Vickysan126@gmail.com>
2025-08-05 21:53:32 +0530

    Updated gallery image with latest changes

---
+++ src/data.js
@@ -0,0 +1,35 @@
+// src/data.js
+const images = [
+  {
+    id: 1,
+    url: 'https://picsum.photos/id/1015/600/400',
+    alt: 'Mountain',
+  },
+  {
+    id: 2,
+    url: 'https://picsum.photos/id/1016/600/400',
+    alt: 'River',
+  },
+  {
+    id: 3,
+    url: 'https://picsum.photos/id/1020/600/400',
+    alt: 'Trees',
+  },
+  {
+    id: 4,
+    url: 'https://picsum.photos/id/1024/600/400',
+    alt: 'Dog',
+  },
+  {
+    id: 5,
+    url: 'https://picsum.photos/id/1035/600/400',
+    alt: 'Beach',
+  },
+  {
+    id: 6,
+    url: 'https://picsum.photos/id/1033/600/400',
+    alt: 'Lake',
+  },
+];
+
+export default images;
